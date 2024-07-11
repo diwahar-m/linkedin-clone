585b23497afdd7033da15988c4c604d5aeafd3af
diwahar-m <msdiwahar5@gmail.com>
2024-07-11 19:37:50 +0530

    checking for global user identity of my github

--- src/app/store.js
+++ src/app/store.js
@@ -9,3 +9,4 @@ export const store = configureStore({
 // ----- REDUX ------------
 // diff slices has diff info in redux. Ex: userSlice, themeSlice.
 // We dispatch an action to change the user in the userSlice.
+// checking fot github.
